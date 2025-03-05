b80e45b74485025826871ff97b160680fd38f0d2
labmahardika <labkommahardika635@gmail.com>
2025-03-05 11:15:59 +0700

    Firts

---
+++ pertemuan-1/list.py
@@ -0,0 +1,22 @@
+# Membuat VariableL
+# String Variable
+namaMhs = "Hello World"
+print(namaMhs)
+print(type(namaMhs))   
+#Variabel Integer
+umurMhs = 20
+print(umurMhs)
+print(type(umurMhs))
+#Variable Float
+ipkMhs = 3.5
+print(ipkMhs)
+print(type(ipkMhs))
+# Variable Boolean
+lulusMhs = 3.5>3.0
+print(lulusMhs)
+print(type(lulusMhs))
+
+# Membuat List
+myList = ["Hello World", 20, 3.5, True]
+print(f"Ini isi List = {myList}") 
+print(type(myList))
