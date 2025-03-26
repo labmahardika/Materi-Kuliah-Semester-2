71413f116fd04cb94d8254b0a659dc4d3536c77a
mhfirdaus <mohamad.firdaus@mahardika.ac.id>
2025-03-26 10:26:20 +0700

    Pertemuan-5

--- Struktur-Data/Pertemuan 4/data.py
+++
@@ -1,6 +0,0 @@
-dataMhs = [
-    {"nama": "Budi", "nim": "12345", "prodi": "Informatika"},
-    {"nama": "Andi", "nim": "12346", "prodi": "Informatika"},
-    {"nama": "Cici", "nim": "12347", "prodi": "Informatika"},
-    ]
-print(type(dataMhs))
